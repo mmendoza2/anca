84a896fd76a85079a5e39abdba5bb0f5fca83d32
Manuel Mendoza <man.m25@gmail.com>
2014-08-26 11:42:32 -0500

    init

---
+++ app/controllers/anca_controller.rb
@@ -0,0 +1,25 @@
+class AncaController < ApplicationController
+
+
+  def home
+      @micrositios = Micrositio.all
+      @eventos = Evento.all
+      @actividades = Actividad.all
+      @users = User.all
+      @user = User.new
+  end
+
+
+
+  def instrucciones
+    @actividades = Actividad.all
+  end
+
+
+
+
+
+
+
+
+end
